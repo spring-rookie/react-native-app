fc481f47fc0e601ae32d09840d0a3fe6575fe5bd
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2019-05-19 22:45:39 +0300

    Add map and flags

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,6 +1,41 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+
+import {
+  StyleSheet,
+  ScrollView,
+  Text,
+  View
+} from 'react-native';
+
+
+
+import MapView from 'react-native-maps';
+
+import image from '../assets/images/flag-pink.png';
+
+
+
+const coordinates = [];
+
+    coordinates.push({
+      key: 0,
+      location: {
+        longitude: 26.1025,
+        latitude: 44.4268
+      }
+    });
+
+    for(let i = 1; i<10; i++) {
+
+      const location = {
+        longitude: coordinates[i-1].location.longitude + (Math.random() * (i%2 === 0 ? -1 : 1)),
+        latitude: coordinates[i-1].location.latitude + (Math.random() * (i%2 === 0 ? -1 : 1)),
+      };
+
+      coordinates.push({ key: i, location });
+
+    }
+
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
@@ -9,13 +44,34 @@ export default class LinksScreen extends React.Component {
 
   render() {
     return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
+      <MapView
+        renderMarker={renderMarker}
+        initialRegion={{
+          longitude: 26.1025,
+          latitude: 44.4268,
+          latitudeDelta: 9.22,
+          longitudeDelta: 4.21,
+        }}
+        style={StyleSheet.absoluteFillObject}>
+
+        { coordinates.map(({ key, location } ) => <MapView.Marker key={key} image={image} coordinate={location} />) }
+
+      </MapView>
     );
   }
+}
+
+function renderMarker({ location }) {
+  return (
+    <MapView.Marker
+      image={image}
+      coordinate={location}
+    >
+      <MapView.Callout>
+        <Text>BiG BiG Callout</Text>
+      </MapView.Callout>
+    </MapView.Marker>
+  );
 }
 
 const styles = StyleSheet.create({
